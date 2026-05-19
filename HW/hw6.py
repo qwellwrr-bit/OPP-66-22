d53d806a991b6ff2874e207315f98a35d470ecd3
qwellwrr-bit <qwellwrr@gmail.com>
2026-05-20 00:08:06 +0600

    add hw6 code

---
+++ HW/hw6.py
@@ -0,0 +1,18 @@
+from colorama import Fore, Style, init
+
+init()
+
+print(Fore.GREEN + "Привет!")
+print(Fore.RED + "Это красный текст")
+print(Style.RESET_ALL)
+
+
+
+
+nums = [2, 7, 11, 15]
+target = 9
+
+for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+        if nums[i] + nums[j] == target:
+            print([i, j])
